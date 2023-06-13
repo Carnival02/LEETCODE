32910a15f65e774d2d26acf20b73ea43f6e9f9ea
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2023-06-13 21:08:10 +0530

    Create value of variable after opr.cpp

---
+++ value of variable after opr.cpp
@@ -0,0 +1,20 @@
+//Question no 2011
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+        
+        for(string &s:operations){
+            if(s[1]=='-'){
+                --x;
+            }else{
+                ++x;
+            }
+        }
+        return x;
+        
+
+        
+    }
+};
