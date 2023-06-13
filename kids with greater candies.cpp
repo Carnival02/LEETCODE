ce1682ca98b53ae1747aa3980e46b686163933b7
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2023-06-13 23:09:02 +0530

    Create kids with greater candies.cpp

---
+++ kids with greater candies.cpp
@@ -0,0 +1,16 @@
+//Question no 1431
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxi =*max_element(candies.begin(),candies.end());
+        vector<bool>ans;
+        for(int i=0;i<candies.size();i++){
+            ans.push_back(candies[i]+extraCandies>=maxi);
+        }
+        return ans;
+
+
+        
+    }
+};
