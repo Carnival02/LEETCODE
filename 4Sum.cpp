f10a68a6dd06301448558a53d883f57c9faea7db
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2023-10-24 20:34:30 +0530

    Create 4Sum.cpp

---
+++ 4Sum.cpp
@@ -0,0 +1,33 @@
+// Time complexity of the soltuion is o(n^3) because we using 3 nested loop and the basic solution having 4 loops and time complexity O(n^4) which give TLE as the error
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        set<vector<int>>set;
+        vector<vector<int>>output;
+         for(int i=0; i<n-3; i++){
+            for(int j=i+1; j<n-2; j++){
+                long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
+                int low = j+1, high = n-1;
+                while(low < high){
+                    if(nums[low] + nums[high] < newTarget){
+                        low++;
+                    }
+                    else if(nums[low] + nums[high] > newTarget){
+                        high--;
+                    }
+                    else{
+                        set.insert({nums[i], nums[j], nums[low], nums[high]});
+                        low++; high--;
+                    }
+                }
+            }
+        }
+        for(auto it : set){
+            output.push_back(it);
+        }
+        return output;
+    }
+};
